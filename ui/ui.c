56d5058191933af0984802f9bef1fae472b59d7f
YGDL <870126805@qq.com>
2023-01-10 21:04:13 +0800

    up

---
+++ ui/ui.c
@@ -0,0 +1,51 @@
+﻿// SquareLine LVGL GENERATED FILE
+// EDITOR VERSION: SquareLine Studio 1.1.0 Beta1
+// LVGL VERSION: 8.2
+// PROJECT: UI
+
+#include "ui.h"
+
+///////////////////// VARIABLES ////////////////////
+lv_obj_t * ui_Screen1;
+lv_obj_t * ui_Label1;
+lv_obj_t * ui_Image1;
+
+///////////////////// TEST LVGL SETTINGS ////////////////////
+
+
+///////////////////// ANIMATIONS ////////////////////
+
+///////////////////// FUNCTIONS ////////////////////
+
+///////////////////// SCREENS ////////////////////
+void ui_Screen1_screen_init(void)
+{
+    ui_Screen1 = lv_obj_create(NULL);
+    lv_obj_clear_flag(ui_Screen1, LV_OBJ_FLAG_SCROLLABLE);      /// Flags
+
+    ui_Label1 = lv_label_create(ui_Screen1);
+    lv_obj_set_width(ui_Label1, LV_SIZE_CONTENT);   /// 1
+    lv_obj_set_height(ui_Label1, LV_SIZE_CONTENT);    /// 1
+    lv_obj_set_x(ui_Label1, 1);
+    lv_obj_set_y(ui_Label1, -63);
+    lv_obj_set_align(ui_Label1, LV_ALIGN_CENTER);
+    lv_label_set_text(ui_Label1, "hhhhhhhhh");
+
+    ui_Image1 = lv_img_create(ui_Screen1);
+    lv_obj_set_width(ui_Image1, LV_SIZE_CONTENT);   /// 1
+    lv_obj_set_height(ui_Image1, LV_SIZE_CONTENT);    /// 1
+    lv_img_set_src(ui_Image1, "E:/Github/lv_port_win_visual_studio/LVGL.Simulator/lvgl/ui/CAT.bmp");
+    lv_obj_set_align(ui_Image1, LV_ALIGN_CENTER);
+    lv_obj_add_flag(ui_Image1, LV_OBJ_FLAG_ADV_HITTEST);     /// Flags
+    lv_obj_clear_flag(ui_Image1, LV_OBJ_FLAG_SCROLLABLE);      /// Flags
+}
+
+void ui_init(void)
+{
+    lv_disp_t * dispp = lv_disp_get_default();
+    lv_theme_t * theme = lv_theme_default_init(dispp, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED),
+                                               false, LV_FONT_DEFAULT);
+    lv_disp_set_theme(dispp, theme);
+    ui_Screen1_screen_init();
+    lv_disp_load_scr(ui_Screen1);
+}
---
+++ ui/ui.h
@@ -0,0 +1,39 @@
+﻿// SquareLine LVGL GENERATED FILE
+// EDITOR VERSION: SquareLine Studio 1.1.0 Beta1
+// LVGL VERSION: 8.2
+// PROJECT: UI
+
+#ifndef _UI_UI_H
+#define _UI_UI_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined __has_include
+#if __has_include("lvgl.h")
+#include "lvgl.h"
+#elif __has_include("lvgl/lvgl.h")
+#include "lvgl/lvgl.h"
+#else
+#include "lvgl.h"
+#endif
+#else
+#include "lvgl.h"
+#endif
+
+extern lv_obj_t * ui_Screen1;
+extern lv_obj_t * ui_Label1;
+extern lv_obj_t* ui_Image1;
+
+
+
+
+
+void ui_init(void);
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif
